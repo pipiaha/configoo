d03c54d6cbdb58d4241345f73b1462fda484a1b5
pipi <393818655@qq.com>
2021-01-28 20:09:09 +0800

    排版修改

--- src/components/header/XlsHeader.vue
+++ src/components/header/XlsHeader.vue
@@ -6,30 +6,51 @@
                 <el-input @change="onMetaParserChange" v-model="dataLine"/>
             </el-form-item>
             <el-divider/>
-            <el-form-item label="服务器列名称所在行">
-                <el-input @change="onMetaParserChange" v-model="server.name"/>
+            <el-form-item label="服务器列名称">
+                <el-row :gutter="10" style="margin-bottom: 8px">
+                    <el-col :span="4">
+                        <el-input @change="onMetaParserChange" type="number" placeholder="所在行"
+                                  v-model="server.name"/>
+                    </el-col>
+                    <el-col :span="12">
+                        <el-input @change="onMetaParserChange" placeholder="提取" v-model="server.namePattern"/>
+                    </el-col>
+                </el-row>
             </el-form-item>
-            <el-form-item label="服务器列名称提取">
-                <el-input @change="onMetaParserChange" v-model="server.namePattern"/>
-            </el-form-item>
-            <el-form-item label="服务器列类型所在行">
-                <el-input @change="onMetaParserChange" v-model="server.type"/>
-            </el-form-item>
-            <el-form-item label="服务器列类型提取">
-                <el-input @change="onMetaParserChange" v-model="server.typePattern"/>
+            <el-form-item label="服务器列类型">
+                <el-row :gutter="10" style="margin-bottom: 8px">
+                    <el-col :span="4">
+                        <el-input @change="onMetaParserChange" type="number" placeholder="所在行"
+                                  v-model="server.type"/>
+                    </el-col>
+                    <el-col :span="12">
+                        <el-input @change="onMetaParserChange" placeholder="提取" v-model="server.typePattern"/>
+                    </el-col>
+                </el-row>
             </el-form-item>
             <el-divider/>
-            <el-form-item label="客户端列名称所在行">
-                <el-input @change="onMetaParserChange" v-model="client.name"/>
+            <el-form-item label="客户端列名称">
+                <el-row :gutter="10" style="margin-bottom: 8px">
+                    <el-col :span="4">
+                        <el-input @change="onMetaParserChange" type="number" placeholder="所在行"
+                                  v-model="client.name"/>
+                    </el-col>
+                    <el-col :span="12">
+                        <el-input @change="onMetaParserChange" placeholder="提取" v-model="client.namePattern"/>
+                    </el-col>
+                </el-row>
             </el-form-item>
-            <el-form-item label="客户端列名称提取">
-                <el-input @change="onMetaParserChange" v-model="client.namePattern"/>
-            </el-form-item>
-            <el-form-item label="客户端列类型所在行">
-                <el-input @change="onMetaParserChange" v-model="client.type"/>
-            </el-form-item>
-            <el-form-item label="客户端列类型提取">
-                <el-input @change="onMetaParserChange" v-model="client.typePattern"/>
+            <el-form-item label="客户端列类型">
+                <el-row :gutter="10" style="margin-bottom: 8px">
+                    <el-col :span="4">
+                        <el-input @change="onMetaParserChange" type="number" placeholder="所在行"
+                                  v-model="client.type"/>
+                    </el-col>
+                    <el-col :span="12">
+                        <el-input @change="onMetaParserChange" placeholder="提取" v-model="client.typePattern"/>
+                    </el-col>
+                </el-row>
+
             </el-form-item>
         </el-form>
     </div>
