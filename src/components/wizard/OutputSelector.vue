5de419d388b719d4820dee10cdfe38f821726c1c
pipi <393818655@qq.com>
2021-02-01 17:57:41 +0800

    修改输出页参数

--- src/components/wizard/OutputSelector.vue
+++ src/components/wizard/OutputSelector.vue
@@ -11,35 +11,57 @@
 
             <el-divider/>
 
-            <el-form-item label="输出内容">
-                <el-row :gutter="10" style="margin-bottom: 8px">
-                    <el-col :xs="24" :sm="24" :md="4">
-                        <el-select v-model="output.records.type" placeholder="选择输出格式">
-                            <el-option v-for="(option,index) in typeOptions" :key="index" :label="option"
-                                       :value="option"/>
-                        </el-select>
-                    </el-col>
-                    <el-col :xs="24" :sm="24" :md="12">
-                        <el-input placeholder="选择输出路径" v-model="output.records.path"/>
-                    </el-col>
-                </el-row>
-            </el-form-item>
-
+            <div>
+                <el-checkbox v-model="output.records.enable" label="输出内容" border style="margin-bottom: 8px"/>
+                <el-form-item label="输出内容" v-if="output.records.enable">
+                    <el-row :gutter="10" style="margin-bottom: 8px">
+                        <el-col :xs="24" :sm="24" :md="4">
+                            <el-select v-model="output.records.type" placeholder="选择输出格式">
+                                <el-option v-for="(option,index) in typeOptions" :key="index" :label="option.name"
+                                           :value="option.name"/>
+                            </el-select>
+                        </el-col>
+                        <el-col :xs="24" :sm="24" :md="8">
+                            <el-input placeholder="选择输出路径" v-model="output.records.path"/>
+                        </el-col>
+                        <el-col :xs="24" :sm="24" :md="10">
+                            <el-checkbox v-model="output.records.useCustomTemplate" style="float: left;padding-left: 5px">自定义</el-checkbox>
+                            <el-select v-if="!output.records.useCustomTemplate" v-model="output.records.template"
+                                       placeholder="选择模板"
+                                       style="">
+                                <el-option v-for="(option,index) in typeOptions" :key="index" :label="option.template"
+                                           :value="option.template"/>
+                            </el-select>
+                            <el-input v-else placeholder="自定义模板文件" v-model="output.records.template"/>
+                        </el-col>
+                    </el-row>
+                </el-form-item>
+            </div>
             <el-divider/>
-
-            <el-form-item label="输出源文件">
-                <el-row :gutter="10" style="margin-bottom: 8px">
-                    <el-col :xs="24" :sm="24" :md="4">
-                        <el-select v-model="output.sources.type" placeholder="选择输出格式">
-                            <el-option v-for="(option,index) in srcOptions" :key="index" :label="option"
-                                       :value="option"/>
-                        </el-select>
-                    </el-col>
-                    <el-col :xs="24" :sm="24" :md="12">
-                        <el-input placeholder="选择输出路径" v-model="output.sources.path"/>
-                    </el-col>
-                </el-row>
-            </el-form-item>
+            <div>
+                <el-checkbox v-model="output.sources.enable" label="输出源文件" border style="margin-bottom: 8px"/>
+                <el-form-item label="输出源文件" v-if="output.sources.enable">
+                    <el-row :gutter="10" style="margin-bottom: 8px">
+                        <el-col :xs="24" :sm="24" :md="4">
+                            <el-select v-model="output.sources.type" placeholder="选择输出格式">
+                                <el-option v-for="(option,index) in srcOptions" :key="index" :label="option.name"
+                                           :value="option.name"/>
+                            </el-select>
+                        </el-col>
+                        <el-col :xs="24" :sm="24" :md="6">
+                            <el-input placeholder="选择输出路径" v-model="output.sources.path"/>
+                        </el-col>
+                        <el-col :xs="24" :sm="24" :md="6">
+                            <el-select v-if="!output.records.useCustomTemplate" v-model="output.records.sources"
+                                       placeholder="选择模板"
+                                       style="float: left;padding-left: 5px">
+                                <el-option v-for="(option,index) in srcOptions" :key="index" :label="option.template"
+                                           :value="option.template"/>
+                            </el-select>
+                        </el-col>
+                    </el-row>
+                </el-form-item>
+            </div>
         </el-form>
     </div>
 </template>
@@ -54,19 +76,47 @@
                 output: {
                     naming: null,
                     records: {
+                        enable: false,
                         type: null,
                         path: null,
+                        template: null,
+                        useCustomTemplate: false,
                     },
                     sources: {
+                        enable: false,
                         type: null,
                         path: null,
+                        template: null,
+                        useCustomTemplate: false,
                     }
                 },
                 typeOptions: [
-                    'csv', 'json', 'sql', 'lua'
+                    {
+                        name: 'csv',
+                        template: 'csv_template',
+                    },
+                    {
+                        name: 'json',
+                        template: 'json_template',
+                    },
+                    {
+                        name: 'sql',
+                        template: 'sql_template',
+                    },
+                    {
+                        name: 'lua',
+                        template: 'lua_template',
+                    },
                 ],
                 srcOptions: [
-                    '.java', '.cs',
+                    {
+                        name: '.java',
+                        template: 'java_src_template',
+                    },
+                    {
+                        name: '.cs',
+                        template: 'csharp_src_template',
+                    },
                 ],
             };
         },
