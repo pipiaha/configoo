63aac5357c857d9b94e426dee4062bfa54b8b6e6
pipi <suntao1022@126.com>
2023-06-08 01:13:36 +0800

    feat: office

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use std::any::type_name;
+
+use office::Excel;
+
 fn main() {
     println!("Hello, world!");
+    let mut excel = Excel::open("file.xlsx").unwrap();
+    let r = excel.worksheet_range("Sheet1").unwrap();
+    for row in r.rows() {
+        println!("row={:?}, row[0]={:?}", row, row[0]);
+    }
 }
