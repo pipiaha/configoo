8cd1356a9a46018c57cf49ca0467d284005fc090
pipiaha <393818655@qq.com>
2023-06-09 18:44:47 +0800

    feat: builder

--- src/core/model.rs
+++ src/core/model.rs
@@ -8,7 +8,7 @@ pub struct ConfigHeader {
 
 // config column unit
 pub struct ConfigColumn {
-    header: *ConfigHeader,
+    header: &ConfigHeader,
     value: String,
     row_index: i32,
 }
@@ -16,12 +16,73 @@ pub struct ConfigColumn {
 // config row
 pub struct ConfigRow {
     row_index: i32,
-    data: Vec<*ConfigColumn>,
+    data: Vec<ConfigColumn>,
+}
+
+pub struct ConfigRowBuilder {
+    row_index: Option<i32>,
+    data: Option<Vec<ConfigColumn>>,
+}
+
+impl ConfigRowBuilder {
+    fn new() -> Self {
+        Self {
+            row_index: None,
+            data: None,
+        };
+    }
+
+    fn set_row_index(mut self, idx: i32) -> Self {
+        self.row_index = Some(idx);
+        self
+    }
+
+    fn add_column(mut self, col: ConfigColumn) -> Self {
+        self.data.get_or_insert_with(Vec::new).push(col);
+        self
+    }
+
+    fn build(self) -> ConfigRow {
+        ConfigRow {
+            row_index: self.row_index.unwrap_or_default(),
+            data: self.data.unwrap_or_default(),
+        }
+    }
 }
 
 // whole config data
 pub struct ConfigTable {
     name: String,
-    data: Vec<*ConfigRow>,
-    header: Vec<*ConfigHeader>,
+    data: Vec<ConfigRow>,
+    header: Vec<ConfigHeader>,
+}
+
+impl ConfigTable {
+    fn new() -> ConfigTable {
+        ConfigTable {
+            name: "".to_string(),
+            data: vec![],
+            header: vec![],
+        };
+    }
+}
+
+pub struct ConfigTableBuilder {
+    name: Option<String>,
+    data: Option<Vec<ConfigRow>>,
+    header: Option<Vec<ConfigHeader>>,
+}
+
+impl ConfigTableBuilder {
+    fn new() -> ConfigTableBuilder {
+        ConfigTableBuilder {
+            name: None,
+            data: None,
+            header: None,
+        }
+    }
+    fn set_name(mut self, name: String) -> ConfigTableBuilder {
+        self.name = Some(name);
+        self;
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 // use office::Excel;
 
+use std::error::Error;
 use std::path::Path;
-use calamine::{open_workbook, Xlsx};
+use calamine::{open_workbook, RangeDeserializerBuilder, Reader, Xlsx};
 
 fn main() {
     println!("Hello, world!");
@@ -11,14 +12,16 @@ fn main() {
     //     println!("row={:?}, row[0]={:?}", row, row[0]);
     // }
 
-    let file_path = Path::new("config/file.xlsx");
+    let file_path = Path::new("config/RhythmMasterConfig.xlsx");
     // 打开 XLSX 文件
     let mut workbook: Xlsx<_> = match open_workbook(file_path) {
-        Ok(wb) => workbook = wb,
-        Err(e) => {
-            eprintln!("Failed to open workbook: {}", e);
+        Ok(wb) => wb ,
+        Err(err) => {
+            eprintln!("can not open file {}", err);
+            return;
         }
     };
+    // .expect("can not open file");
     // 获取第一个工作表
     let sheet = match workbook.worksheet_range_at(0) {
         Some(Ok(sheet)) => sheet,
@@ -34,4 +37,24 @@ fn main() {
     for row in sheet.rows() {
         println!("row={:?}, row[0]={:?}", row, row[0]);
     }
+
+
 }
+
+// fn example() -> Result<(), Error> {
+//     let path = format!("{}/tests/temperature.xlsx", env!("CARGO_MANIFEST_DIR"));
+//     let mut workbook: Xlsx<_> = open_workbook(path)?;
+//     let range = workbook.worksheet_range("Sheet1")
+//         .ok_or(Error::Msg("Cannot find 'Sheet1'"))??;
+//
+//     let mut iter = RangeDeserializerBuilder::new().from_range(&range)?;
+//
+//     if let Some(result) = iter.next() {
+//         let (label, value): (String, f64) = result?;
+//         assert_eq!(label, "celsius");
+//         assert_eq!(value, 22.2222);
+//         Ok(())
+//     } else {
+//         Err(From::from("expected at least one record but got none"))
+//     }
+// }
